beaf02680c0348277a4f6a4b79af5f0887d73f5e
Deante Taylor <deante16@hotmail.co.uk>
2021-02-08 04:06:16 -0400

    Updated Hello World

--- Large Scale Programming/src/org/howard/edu/lsp/assignment1/HelloWorld.java
+++ Large Scale Programming/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -3,11 +3,8 @@ package org.howard.edu.lsp.assignment1;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
 		System.out.println("Hello LSP, from Deante Taylor!");
-=======
-		System.out.println("Hello LSP from Deante Taylor!");
->>>>>>> branch 'main' of https://github.com/DMT-Codes/xaikaywork1.git
+
 
 	}
 
