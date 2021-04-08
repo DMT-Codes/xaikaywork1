a7bb02c45c11ece34f829f9a45a55d321d081372
Deante Taylor <deante16@hotmail.co.uk>
2021-04-07 23:51:23 -0400

    Assignment 6

---
+++ Large Scale Programming/src/org/howard/edu/lsp/assignment6/integerset/IntegerSetException.java
@@ -0,0 +1,14 @@
+package org.howard.edu.lsp.assignment6.integerset;
+
+/**
+ * 
+ * This class implements the custom IntegerSetException exception
+ * 
+ * @author Deante Taylor 
+ *
+ */
+public class IntegerSetException extends Exception {
+    public IntegerSetException(String excep) {
+        super(excep);
+    }
+}
